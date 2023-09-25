209606e2f15e4bf1660d02305f3ecfa9c839dc06
fbarzola <135671763+fbarzola@users.noreply.github.com>
2023-09-24 22:29:22 -0300

    Add files via upload

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,20 +1,44 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import img from "./3D_Store.png";
 import './header.css';
+import { Typography, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    poster: {
+      fontSize: 60,
+      color:'black',
+    },
+  },
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          poster: 'h1',
+        },
+      },
+    },
+  },
+});
 
 const Header = () => {
   return (
-    <header>
-      <div className="logo-container">
-        <img src={img} alt="logo-3D-Store" className="logo" />
-      </div>
-      <div className="text-container">
-        <h1>Bienvenido a 3D-Store</h1>
-        <p>Explore Nuestros productos 3D</p>
-      </div>
-    </header>
+    <ThemeProvider theme={theme}>
+      <header>
+        <div className="logo-container">
+          <img src={img} alt="logo-3D-Store" className="logo" />
+        </div>
+        <div className="text-container">
+          <Typography variant="poster">Bienvenido a 3D-Store</Typography>
+          <p>Explore Nuestros productos 3D</p>
+        </div>
+      </header>
+    </ThemeProvider>
   );
 };
 
