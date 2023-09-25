af65a829c29bec046b01c573b078c4f07c11cbb9
fbarzola <135671763+fbarzola@users.noreply.github.com>
2023-09-24 23:17:47 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Header from './components/Header/header';
-import NavBar from './Navbar/NavBar';
+import NavBar from './NavBar/NavBar';
 import Cardproduct from './components/CardPorduct/Cardproduct';
 import { private_createTypography } from '@mui/material';
 import "./App.css";
