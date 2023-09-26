370ae5738599ef924d0ef30454e4641d042a8dc6
fbarzola <135671763+fbarzola@users.noreply.github.com>
2023-09-26 01:19:41 -0300

    Add files via upload

---
+++ src/components/CardPorduct/Cardproduct.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+const Cardproduct = (props) => {
+  return (
+    <Card sx={{ maxWidth: 300 }}>
+        
+      <CardMedia
+        component="img"
+        image={props.img}
+        alt="Impresora Endler Pro"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.price}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Agregar a Carrito</Button>
+        <></>
+        <Button size="small">Ver Espec.</Button>
+      </CardActions>
+    </Card>
+    
+  );
+}
+
+export default Cardproduct;
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+//import './ItemListContainer.css';
+import { Padding } from '@mui/icons-material';
+
+const ItemListContainer = () => {
+  return (
+    
+    <div
+
+      style={{ backgroundColor: "aliceblue",
+      padding: "1px",
+      width: "100%",
+      height: "30px",
+      display: "flex",
+      justifyContent: "flex-start", 
+      alignItems: "center",
+      fontSize: "5px", 
+      margin: "0",
+      }}
+      ><h1>Listado de Productos/Impresoras3D</h1>
+      
+    </div>
+  )
+}
+
+export default ItemListContainer
