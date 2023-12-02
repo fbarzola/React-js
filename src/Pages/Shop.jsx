30bad372819c5c7782c79728fd0811c57d0f46e4
fbarzola <135671763+fbarzola@users.noreply.github.com>
2023-12-02 03:47:03 -0300

    Add files via upload

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,57 +1,82 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import PaperShop from '../components/Paper/PaperShop';
-import { AuthProvider, useAuth } from '../components/Header/AuthContext';
-import { CartProvider } from '../components/Paper/CartContext';
+import { useAuth } from '../components/Header/AuthContext';
 import { useCart } from '../components/Paper/CartContext';
 import { db } from '../firebase/firebaseConfig';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, doc, getDoc } from 'firebase/firestore';
 
 const Shop = ({ cart, removeFromCart }) => {
   const navigate = useNavigate();
-  const { isUserLoggedIn } = useAuth();
+  const { isUserLoggedIn, user } = useAuth();
   const { dispatch } = useCart();
+  const [orderId, setOrderId] = useState(null);
 
-  const cartData = {
-    nombre: "Nombre",
-    apellido: "Apellido",
-    cantidad: "Cantidad",
-    dni: "DNI",
-    usuario: "Usuario",
-    email: "Email",
-    fechaCompra: "Fecha de Compra",
-    precioTotal: "Precio Total",
-    producto: "Producto",
+  const getUserData = async () => {
+    if (isUserLoggedIn) {
+      try {
+        const userDocRef = doc(db, 'Usuarios', user.usuario);
+        const userDoc = await getDoc(userDocRef);
+        return userDoc.data() || {};
+      } catch (error) {
+        console.error('Error al obtener datos del usuario: ', error);
+        return {};
+      }
+    } else {
+      return {};
+    }
   };
 
   const handleFinalizarCompra = async () => {
-    setTimeout(async () => {
-      if (isUserLoggedIn) {
-        try {
-          const docRef = await addDoc(collection(db, 'Compras'), cartData);
-          const orderId = docRef.id;
-
-          alert(`Compra Exitosa!! Verifique su e-mail, Su numero de order es: ${orderId}`);
-          dispatch({ type: 'UPDATE_CART', payload: [] });
-          navigate("/");
-        } catch (error) {
-          console.error('Error al guardar la compra: ', error);
-          alert('Hubo un error al procesar la compra. Por favor, inténtelo de nuevo.');
-        }
-      } else {
-        alert('Primero debes registrarte');
-        navigate('/register');
+    if (isUserLoggedIn) {
+      try {
+        const isoDate = new Date().toISOString();
+  
+        const userData = await getUserData();
+  
+        const totalPrecio = cart.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
+  
+        const productosCompra = cart.map((item) => ({
+          name: item.product.Title || '',
+          quantity: item.quantity || 0,
+          price: (item.product.price * item.quantity).toFixed(2) || '0.00',
+        }));
+  
+        const purchaseDocRef = await addDoc(collection(db, 'Compras'), {
+          nombre: userData.nombre || 'Nombre no disponible',
+          apellido: userData.apellido || 'Apellido no disponible',
+          cantidad: cart.reduce((acc, item) => acc + item.quantity, 0),
+          dni: userData.dni || 'DNI no disponible',
+          usuario: userData.usuario || 'Usuario no disponible',
+          email: userData.email || 'Email no disponible',
+          fechaCompra: isoDate,
+          precioTotal: totalPrecio.toFixed(2),
+          producto: productosCompra,
+        });
+  
+        setOrderId(purchaseDocRef.id);
+  
+        dispatch({ type: 'UPDATE_CART', payload: [] });
+        navigate('/');
+  
+        alert(`Compra Exitosa!! Verifique su e-mail, Su numero de order es: ${purchaseDocRef.id}`);
+      } catch (error) {
+        console.error('Error al guardar la compra: ', error);
+        alert('Hubo un error al procesar la compra. Por favor, inténtelo de nuevo.');
       }
-    }, 2000);
+    } else {
+      alert('Primero debes registrarte');
+      navigate('/register');
+    }
   };
 
   return (
     <div style={{ marginTop: 50, marginLeft: 100 }}>
       <h1>Carrito de Compras</h1>
-      <h1></h1>
+        <h1></h1>
       <Button
         style={{
           display: 'block',
@@ -59,9 +84,11 @@ const Shop = ({ cart, removeFromCart }) => {
           marginTop: '40px',
           textAlign: 'center',
           color: 'lightcoral',
-          borderColor: 'lightcoral'
+          borderColor: 'lightcoral',
         }}
-        variant='outlined' onClick={() => navigate("/")}>
+        variant='outlined'
+        onClick={() => navigate('/')}
+      >
         Seguir Comprando
       </Button>
       <h1></h1>
@@ -72,9 +99,11 @@ const Shop = ({ cart, removeFromCart }) => {
           marginLeft: '2px',
           marginTop: '10px',
           textAlign: 'center',
-          backgroundColor: 'lightcoral'
+          backgroundColor: 'lightcoral',
         }}
-        variant='contained' onClick={handleFinalizarCompra}>
+        variant='contained'
+        onClick={handleFinalizarCompra}
+      >
         Finalizar Compra!
       </Button>
 
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -149,7 +149,6 @@ const Header = () => {
   }, [auth]);
 
 
-
   return (
     <ThemeProvider theme={theme}>
       <header>
